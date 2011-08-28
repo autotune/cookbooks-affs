3cc39caf89dcbcade36e88b78bd615cd8d1dfd3e
Sean OMeara <someara@opscode.com>
2011-08-28 00:51:55 -0400

    adding stuff for commit

---
+++ yum/recipes/epel.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: yum
+# Recipe:: epel
+#
+# Copyright 2011, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+elmajor = node[:platform_version].to_i
+arch = node[:kernel][:machine]
+
+# works as of Jul 22 2011
+if elmajor == 5 then
+   rev=4
+elsif elmajor == 6 then 
+  rev=5
+else
+  rev=1
+end
+
+execute "installing EPEL repo" do
+  not_if "rpm -qa | grep epel-release"
+  command "rpm -Uvh http://download.fedora.redhat.com/pub/epel/#{elmajor}/#{arch}/epel-release-#{elmajor}-#{rev}.noarch.rpm"
+end
+
+
